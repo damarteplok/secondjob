3973d04399bfb8c7169dd5b17b53ccb550f8bb78
damar huda <damarhuda@live.com>
2019-02-24 22:10:09 +0700

    second

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <section class="posts-list">
+      <PostPreview
+        id="1"
+        thumbnail="http://0.soompi.io/wp-content/uploads/2016/03/31204343/twice-tzuyu-2.jpg"
+        title="Hello there"
+        previewText="This my first post"
+      />
+      <PostPreview
+        id="2"
+        thumbnail="http://0.soompi.io/wp-content/uploads/2016/03/31204343/twice-tzuyu-2.jpg"
+        title="Hello there2"
+        previewText="This my first post2"
+      />
+      <PostPreview
+        id="3"
+        thumbnail="http://0.soompi.io/wp-content/uploads/2016/03/31204343/twice-tzuyu-2.jpg"
+        title="Hello there3"
+        previewText="This my first post3"
+      />
+      <PostPreview
+        id="4"
+        thumbnail="http://0.soompi.io/wp-content/uploads/2016/03/31204343/twice-tzuyu-2.jpg"
+        title="Hello there4"
+        previewText="This my first post4"
+      />
+    </section>
+  </div>
+</template>
+<script>
+import PostPreview from "@/components/Posts/PostPreview";
+export default {
+  components: {
+    PostPreview
+  }
+};
+</script>
+
+<style scoped>
+.posts-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ pages/about/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="about-page">
+        <h1>This is my absolutly fantastic "about me" page</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur corrupti magnam tempora fugit a maiores sunt blanditiis quaerat corporis hic dicta molestiae dolorum enim, sapiente explicabo optio, omnis saepe! Ab.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime dolorem impedit autem tempora excepturi voluptates dignissimos aspernatur totam ratione, nesciunt explicabo, incidunt recusandae. Ab et placeat cupiditate reprehenderit itaque aliquam!</p>
+    </div>
+</template>
+
+<style scoped>
+    .about-page {
+        text-align: center;
+        padding: 30px;
+    }
+</style>
